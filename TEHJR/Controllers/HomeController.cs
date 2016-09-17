6ad5cd8a71e9d9b10372bb2ff462cab36c96b42c
HermantNET <hermant.edu@gmail.com>
2016-09-17 14:23:04 -0400

    Initial commit
    Uploaded site

---
+++ TEHJR/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TEHJR.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Title = "About";
+            ViewBag.Message = "Thomas E. Herman Jr.";
+
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Title = "Contact";
+            ViewBag.Message = "Thomas E. Herman Jr.";
+
+            return View();
+        }
+    }
+}
---
+++ TEHJR/Scripts/getPartialViews.js
@@ -0,0 +1,31 @@
+﻿var selector = $("#content");
+var loading = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" class="uil-ring"><circle cx="50" cy="50" r="10" stroke-dasharray="53.36281798666926 87.9645943005142" stroke="#f2b736" fill="none" stroke-width="5"><animateTransform attributeName="transform" type="rotate" values="0 50 50;180 50 50;360 50 50;" keyTimes="0;0.5;1" dur="1s" repeatCount="indefinite" begin="0s"></animateTransform></circle></svg>`;
+
+function loadingRing() {
+    selector.html(loading);
+    $(".uil-ring").fadeIn("slow", () => {});
+}
+
+function Home() {
+    loadingRing();
+    setTimeout(function () {
+        $.get("", (data) => {
+            selector.html(data);
+            location.hash = "/";
+        });
+    }, 5000);
+}
+function About() {
+    loadingRing();
+    $.get("Home/About", (data) => {
+        selector.html(data);
+        location.hash = "/Home/About";
+    });
+}
+function Contact() {
+    loadingRing();
+    $.get("Home/Contact", (data) => {
+        selector.html(data);
+        location.hash = "/Home/Contact";
+    });
+}
